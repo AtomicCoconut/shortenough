deedc85e23b6b8378fe3b13d73e6c03bf3061811
AtomicCoconut <66516943+AtomicCoconut@users.noreply.github.com>
2023-04-04 21:10:37 +0200

    Add gin routing and basic routes

--- internal/server.go
+++ internal/server.go
@@ -1,5 +1,18 @@
 package internal
 
-func StartServer() {
+import (
+	"github.com/atomiccoconut/shortenough/internal/routes"
+	"github.com/gin-gonic/gin"
+)
 
+func StartServer() {
+	router := gin.Default()
+
+	router.GET("/:id", routes.ServeShort)
+	router.POST("/", routes.CreateShort)
+
+	err := router.Run()
+	if err != nil {
+		panic(err)
+	}
 }
